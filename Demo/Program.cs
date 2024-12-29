967594613e1f4bccbed5b602817a66b946027c65
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-29 16:21:41 +0200

    Part 02 What are Access Modifiers

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -12,6 +12,19 @@ namespace Demo
                  * Just Import This Library In The Project That You Nedd To Use Class Library Inside It.
              */
             #endregion
+
+            #region Part 02 What are Access Modifiers
+            //Specify The Accecibility Scope OF Items Like Classes,Structs,Enums,Interfaces,Methods,Properties,Fields,Constructors,.....
+            /*
+                 * 1- Private 
+                 * 2- Private Protected [C# 7.3 Feature] [Has Inheritence Scope]
+                 * 3- Protected                          [Has Inheritence Scope]
+                 * 4- Internal
+                 * 5- Internal Protected                 [Has Inheritence Scope]
+                 * 6- Public
+                 * 7- File
+             */
+            #endregion
         }
     }
 }
