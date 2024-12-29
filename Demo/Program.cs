fd6fd8cf7d5aabcec7f73b93d662a44538bcda36
ESLAMELSAADANI <144673435+ESLAMELSAADANI@users.noreply.github.com>
2024-12-29 16:03:34 +0200

    Part 01 What Is Class Library

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace Demo
+﻿using System.Security.AccessControl;
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01 What Is Class Library
+            /*
+                 * It's Just Library That We Write C# Code Inside It
+                 * To use This code In Many Projects/Apps Without Rewrite This Code Again In Every Project,
+                 * Just Import This Library In The Project That You Nedd To Use Class Library Inside It.
+             */
+            #endregion
         }
     }
 }
